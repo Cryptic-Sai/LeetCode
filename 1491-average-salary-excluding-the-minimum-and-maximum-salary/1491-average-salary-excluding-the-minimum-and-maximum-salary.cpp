3c7ca15f765b5e2925e5729ef6f5575a03a0fb1c
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-05-01 16:35:59 +0530

    Time: 5 ms (23.02%), Space: 7.1 MB (84.06%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -12,7 +12,9 @@ public:
             s+=i;
         }
         return (s-maxi-mini)*1.0/(salary.size()-2);
-    }
+    } 
+
+    
 //     int mi=INT_MAX,ma=INT_MIN,s=0;
 //         for(auto i:salary)mi=min(mi,i),ma=max(ma,i),s+=i;
 //         return (s-ma-mi)*1.0/(salary.size()-2);
