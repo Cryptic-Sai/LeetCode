8206a41a85ecf89dbedfca485a17926c2490da06
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-05-02 10:46:23 +0530

    Time: 3 ms (92.77%), Space: 10.3 MB (45.94%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -8,5 +8,32 @@ public:
         ans= ans*nums[i]/abs(nums[i]);
     }
     return ans;
-    }
+        
+    // int signFunc(x) =0;
+    // if(x>0){
+    //     return 1;
+    // }else if(x<0)
+    //         return -1;       
+    // else{
+    //     return 0;
+    // }
+        
+//         int ans =1;
+//         for(int i=0; i<nums.size(); i++){
+//             if(nums[i] == 0){
+//                 return 0;
+                
+//                 ans = ans*nums[i]/abs(nums[i]);
+//             }
+//             else if (nums[i] >0){
+//                 return 1;
+//             }
+//             else{
+//                 return -1;
+//             }
+                
+            
+            
+//         }return ans;
+}
 };
