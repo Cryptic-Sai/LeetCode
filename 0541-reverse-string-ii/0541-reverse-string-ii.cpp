52108c39b8db970d2ace1173d85fd50fa526828e
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-05-19 17:32:06 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (93.73%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.cpp
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -2,13 +2,24 @@ class Solution {
 public:
     string reverseStr(string s, int k) {
     
-        int i = 0, n = s.length();
-        while(i<n) {
-            int j = min(i+k-1, n-1);
-            int nexti = j+k+1;
-            while(i<j) swap(s[i++], s[j--]);
-            i = nexti;
+        // int i = 0, n = s.length();
+        // while(i<n) {
+        //     int j = min(i+k-1, n-1);
+        //     int nexti = j+k+1;
+        //     while(i<j) swap(s[i++], s[j--]);
+        //     i = nexti;
+        // }
+        // return s;
+        
+        int l =0;
+        int e = min(k , (int)s.length());
+        
+        while(l< s.length()){
+            reverse(s.begin() + l, s.begin() +e );
+            l += 2*k;
+            e = min(l+k , (int)s.length());
         }
         return s;
     }
+    
 };
