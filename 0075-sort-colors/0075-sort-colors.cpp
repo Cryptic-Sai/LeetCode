9d67edf740f03b2e05d45fc82f07d74c8bc17e1a
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-06-04 16:17:45 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (86.29%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,50 @@
+// Three Pointers
+// Dutch national Flag Algorithm
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+//         int low = 0;
+//         int high = nums.size()-1;
+//         int mid = 0;
+
+//         // Moving till all elements are sorted.
+//         while(mid <= high) {
+//             switch (nums[mid]) {
+
+//         case 0:
+//             swap(nums[low++], nums[mid++]);
+//             break;
+
+//         case 1:
+//             mid++;
+//             break;
+
+//         case 2:
+//             swap(nums[mid], nums[high--]);
+//             break;
+        
+//             }
+//         }
+        int l=0;
+        int h = nums.size()-1;
+        int m = 0;
+        
+        while(m<=h){
+            switch(nums[m]){
+                    
+                case 0:
+                    swap(nums[l++] , nums[m++]);
+                    break;
+                case 1:
+                    m++;
+                    break;
+                    
+                case 2:
+                    swap(nums[m] , nums[h--]);
+                    break;
+                                        
+            }
+        }
+    }
+};
