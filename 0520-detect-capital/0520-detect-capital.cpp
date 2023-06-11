44afdc0f3bcba8938d1722818d46ce83d0c09aee
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-06-12 00:30:47 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (23.03%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int count =0;
-        if (word.size() == 1)
+        int count=0;
+        if(word.size()==1)
             return true;
         
-        for(int i =0; i<word.length(); i++)
+        for(int i=0; i<word.size(); i++)
             if(isupper(word[i]))
-               count++;
-            
-            if(count==1 && isupper(word[0]))
+                count++;
+        
+        if(count==1 && isupper(word[0]))
             return true;
-            
-            
-            if(count==0 || count==word.size())
+        if(count==0 || count==word.size())
             return true;
-         else
+        else
             return false;
-     
-   }
+    }
 };
