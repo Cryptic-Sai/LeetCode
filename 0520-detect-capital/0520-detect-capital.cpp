e12add00946102b91bd6915faa98cde2e263d52b
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-06-13 11:39:45 +0530

    Time: 2 ms (44.24%), Space: 5.9 MB (99.91%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int count=0;
-        if(word.size()==1)
+        
+//         Initialize count=0
+// if count equals 0 means all are small chars
+// if count equals size of word then all are capital
+// if count equals 1 then check is it the first char which is capital
+// if none of the conditions are true then return false
+        int count =0;
+        if(word.size() == 1)
             return true;
         
         for(int i=0; i<word.size(); i++)
             if(isupper(word[i]))
                 count++;
         
-        if(count==1 && isupper(word[0]))
+        if(count == 1 && isupper(word[0]))
             return true;
-        if(count==0 || count==word.size())
+        
+        if(count== word.size() || count == 0 )
             return true;
+        
         else
             return false;
     }
