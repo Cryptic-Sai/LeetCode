4d135e7b8b3f940aad6ae4354d31cfbd1333cd97
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-06-22 00:15:59 +0530

    Time: 4 ms (40.15%), Space: 6.2 MB (18.78%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,90 @@
+class Solution {
+public:
+//     bool originVerifier(int x,int y){
+//         if(x == 0 && y == 0)
+// 			return true;
+// 		return false;
+//     }
+    
+    bool isRobotBounded(string instructions) {
+        int x=0 ,y=0;
+        char current_direction = 'N';
+        for(int i=0; i<instructions.length(); i++){
+            if(instructions[i] == 'G'){
+                y+= current_direction == 'N' ? 1:0;
+                x+= current_direction == 'W' ? -1:0;
+                y+= current_direction == 'S' ? -1:0;
+                x+= current_direction == 'E' ? 1:0;                
+            }
+            else{
+                char incoming = instructions[i];
+                if(current_direction == 'N'){
+                    current_direction = incoming == 'L' ? 'W' : 'E';
+                }
+                else if(current_direction == 'W'){
+                    current_direction = incoming == 'L' ? 'S' : 'N';
+                }
+                else if(current_direction == 'S'){
+                    current_direction = incoming == 'L' ? 'E' : 'W';
+                }else{
+                    current_direction = incoming == 'L' ? 'N' : 'S' ;                       
+                }
+            }
+        }
+        if(x==0 && y==0 || current_direction != 'N'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
+
+
+// class Solution {
+// public:
+//     //returns true if the co-ordinates are at origin
+//     bool originVerifier(int x,int y){
+//         if(x == 0 && y == 0)
+// 			return true;
+// 		return false;
+//     }
+	
+//     bool isRobotBounded(string instructions) {
+//         //given
+//         char current_dir = 'N';
+//         //co-ordinate points
+//         int x = 0, y= 0;
+//         for(int i=0;i<instructions.length();i++){ 
+//             //Case1: If 'G' is in the instruction string
+//             if(instructions[i] == 'G'){
+//                 y += current_dir =='N' ? 1 : 0;
+// 				y += current_dir =='S' ? -1 : 0;
+// 				x += current_dir == 'E' ? 1 : 0;
+// 				x += current_dir == 'W' ? -1 : 0;
+//             }
+//             //case2: If we found 'directions'
+//             else{
+//                 char incoming = instructions[i];
+//                 if(current_dir == 'N'){
+//                     current_dir = incoming == 'L' ? 'W' : 'E';
+//                 }
+//                 else if(current_dir == 'W'){
+//                     current_dir = incoming == 'L' ? 'S' : 'N';
+//                 }
+//                 else if(current_dir == 'S'){
+//                     current_dir = incoming == 'L' ? 'E' : 'W';
+//                 }
+//                 else{
+//                     current_dir = incoming == 'L' ? 'N' : 'S';
+//                 }
+//             } 
+            
+//         }
+//         //if the robo is at origin or the current direction is not north than return true
+//         if(originVerifier(x,y) || current_dir!='N')
+//             return true;
+        
+//         return false;
+//     }
+// };
