e28154d44490e399843665fca934161126ce5a61
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-06-24 22:35:22 +0530

    Time: 3 ms (46.95%), Space: 8.4 MB (8.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -26,25 +26,27 @@ public:
         
 //             }
 //         }
-        int l=0;
-        int h = nums.size()-1;
-        int m = 0;
+//         int l=0;
+//         int h = nums.size()-1;
+//         int m = 0;
         
-        while(m<=h){
-            switch(nums[m]){
+//         while(m<=h){
+//             switch(nums[m]){
                     
-                case 0:
-                    swap(nums[l++] , nums[m++]);
-                    break;
-                case 1:
-                    m++;
-                    break;
+//                 case 0:
+//                     swap(nums[l++] , nums[m++]);
+//                     break;
+//                 case 1:
+//                     m++;
+//                     break;
                     
-                case 2:
-                    swap(nums[m] , nums[h--]);
-                    break;
+//                 case 2:
+//                     swap(nums[m] , nums[h--]);
+//                     break;
                                         
-            }
-        }
+//             }
+//         }
+        
+        sort(nums.begin(), nums.end());
     }
 };
