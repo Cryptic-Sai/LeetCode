0cf6439db7163192b31f90f6c383c9a7b9b6c343
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-07-02 01:09:32 +0530

    Time: 266 ms (35.48%), Space: 138 MB (26.29%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,7 +1,7 @@
 // class Solution {
 // public:
 //     vector<int> getAverages(vector<int>& nums, int k) {
-//         long long int n= nums.size();
+//         int n= nums.size();
 //         long long int pre[n],suff[n];
         
 //         pre[0] = nums[0];
@@ -20,8 +20,8 @@
 //                 ans.push_back(-1);
 //             }
 //             else{
-//                 // long long int sum=(long long int)((suff[i-k]-suff[i]) + (pre[i+k]-pre[i]) +nums[i]);
-//                 long long int sum=(long long int)((suff[i-k]-suff[i]) + (pre[i+k]-pre[i]) + nums[i]);
+//                 long long int sum=(long long int)((suff[i-k]-suff[i]) + (pre[i+k]-pre[i]) +nums[i]);
+//                 // long long int sum=(long long int)((suff[i-k]-suff[i]) + (pre[i+k]-pre[i]) + nums[i]);
 //                 sum = sum / (2*k+1);
 //                 ans.push_back(sum);
 //             }
