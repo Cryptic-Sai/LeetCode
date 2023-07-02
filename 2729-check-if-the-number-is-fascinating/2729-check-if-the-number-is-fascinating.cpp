745436b03996a1558b61807a0c9c6ae653fc0464
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-07-02 15:23:31 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (78.51%) - LeetHub

--- 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.cpp
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.cpp
@@ -3,6 +3,8 @@ public:
     bool isFascinating(int n) {
         string str= to_string(n)+to_string(2*n)+to_string(3*n);
         sort(str.begin(),str.end());
-        return str == "123456789";
+         if(str == "123456789") 
+             return true;
+        return false;
     }
 };
