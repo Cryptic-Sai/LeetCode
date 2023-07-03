64631316f0188292c87f9f80fae9fdb8f3296e9a
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-07-04 00:32:56 +0530

    Time: 4 ms (54.81%), Space: 6.9 MB (85.37%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -0,0 +1,66 @@
+// class Solution {
+// public:
+//     bool buddyStrings(string s, string goal) {
+//         int i=0;
+//         int j= i+1;
+        
+//         string str = swap(s[i] , s[j]);
+//         if(str == goal){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     }
+// };
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if(s.length() != goal.length()){
+            return false;
+        }
+        
+        map<char,int> mp;
+        
+        for(auto i:s){
+            mp[i]++;
+        }
+        for(auto i:goal){
+            mp[i]--;
+        }
+        for(auto i:mp){
+            if(i.second != 0){
+                return false;
+            }
+        }
+        
+        
+        int cnt = 0;
+        for(int i=0; i<s.length(); i++){
+            if(goal[i] != s[i]){
+                cnt++;
+            }
+            
+            if(cnt > 2){
+                break;
+            }
+        }
+        
+        if(cnt > 2 || cnt==1){
+            return false;
+        }
+        
+        mp.clear();
+        if(cnt == 0){
+            for(auto i:s){
+                if(mp.find(i) != mp.end()){
+                    return true;
+                }
+                mp[i]++;
+            }
+            return false;
+        }
+        
+        return true;
+        
+    }
+};
