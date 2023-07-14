812a7f3cd8215bfcd20bfd9a1c56b01651c9e35f
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-07-14 11:27:34 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (42.04%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -22,18 +22,42 @@ public:
         }
         return len;
     }
-    ListNode* middleNode(ListNode* head) {
-        int len = getlength(head);
-        int ans = (len)/2;
-//         Node return karna hai uska number nahi so 
-//         for that we will make temo node
-        ListNode* temp = head;
-        int count =0;
-        while(count <ans){
-            temp = temp ->next;
-            count ++;
+    
+    ListNode* getMiddle(ListNode* head){
+        if(head == NULL || head ->next == NULL){
+            return head;
         }
-        return temp;
+//         Not Nexesary but we can put condition for two elemets in linked list
+        if(head -> next -> next == NULL){
+            return head -> next;
+        }
+        ListNode* fast = head ->next;
+        ListNode* slow = head;
+        
+        while(fast != NULL){
+            fast = fast -> next;
+            if(fast !=NULL){
+                fast = fast -> next;
+            }
+            slow = slow -> next;
+        }
+        return slow;
+    }
+    
+    ListNode* middleNode(ListNode* head) {
+        
+        return getMiddle(head);
+//         int len = getlength(head);
+//         int ans = (len)/2;
+// //         Node return karna hai uska number nahi so 
+// //         for that we will make temp node which we have to make it to the middle node
+//         ListNode* temp = head;
+//         int count =0;
+//         while(count <ans){
+//             temp = temp ->next;
+//             count ++;
+//         }
+//         return temp;
     }
     
 };
