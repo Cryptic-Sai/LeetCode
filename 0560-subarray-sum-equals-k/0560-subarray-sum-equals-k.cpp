a62f30ccbeb48081bc0d31b2433ffc76aa614f12
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-07-28 01:55:12 +0530

    Time: 93 ms (86.18%), Space: 43.8 MB (45.05%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int prefix[n];
+//         int n = nums.size();
+//         int prefix[n];
         
-         prefix[0] = nums[0];
+//          prefix[0] = nums[0];
         
-        for(int i=1; i<n; i++){
-            prefix[i] = nums[i] + prefix[i-1];
-        }
-        unordered_map<int,int> mp;
-        int count =0;
-        for(int i=0; i<n; i++){
-            if(prefix[i]==k)
-                count++;
+//         for(int i=1; i<n; i++){
+//             prefix[i] = nums[i] + prefix[i-1];
+//         }
+//         unordered_map<int,int> mp;
+//         int count =0;
+//         for(int i=0; i<n; i++){
+//             if(prefix[i]==k)
+//                 count++;
             
-            if(mp.find(prefix[i] -k) != mp.end())
-            {
-                count += mp[prefix[i] - k]; 
-            }
+//             if(mp.find(prefix[i] -k) != mp.end())
+//             {
+//                 count += mp[prefix[i] - k]; 
+//             }
             
-            mp[prefix[i]]++;
+//             mp[prefix[i]]++;
                 
+//             }
+//         return count;
+//         }
+    
+        int sum =0;
+        int count =0;
+        map<int, int>mp;
+        
+        for(int i=0; i< nums.size(); i++){
+            sum += nums[i];
+            
+            if(sum == k){
+                count ++;
             }
-        return count;
+            if(mp.find(sum -k) != mp.end()){
+                count += mp[sum-k];
+            }
+            mp[sum]++;
         }
+        return count;
+    
         // int i=0, j=0;
         // int count=0;
         // int sum =0;
@@ -39,4 +57,5 @@ public:
         //     }
         // }
         // return count;
+}
 };
