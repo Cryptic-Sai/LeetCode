9d5d8e0c469f2d05239517b6a1abdaa0c7b4d524
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-07-29 10:01:56 +0530

    Time: 3 ms (84.96%), Space: 9.5 MB (54.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,25 +3,40 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
     if(!matrix.size()) return false;
 
-    int m = matrix.size();
-    int n = matrix[0].size();
+      int m = matrix.size();
+      int n  = matrix[0].size();
 
-    int low = 0;
-    int high = (m*n -1);
+//     int low = 0;
+//     int high = (m*n -1);
 
-    while(low<=high){
-        int mid = low +(high - low) /2;
-        if(matrix[mid/n][mid % n] == target){
-            return true;
+//     while(low<=high){
+//         int mid = low +(high - low) /2;
+//         if(matrix[mid/n][mid % n] == target){
+//             return true;
+//         }
+//         else if(matrix[mid/n][mid % n] < target){
+//             low = mid+1;
+//         }  
+//         else{
+//             high = mid-1;
+//         }  
+
+//     }
+//     return false;
+        
+        int l =0;
+        int h = (m*n -1);
+        while( l<= h){
+            int mid = l + (h - l)/2;
+//             Index of imaginary mid will be
+            if(matrix[mid/n][mid%n] == target){
+                return true;
+            }else if(matrix[mid/n][mid%n] < target){
+                l = mid +1;
+            }else{
+                h =mid -1;
+            }
         }
-        else if(matrix[mid/n][mid % n] < target){
-            low = mid+1;
-        }  
-        else{
-            high = mid-1;
-        }  
-
-    }
-    return false;
+        return false;
   }
 };
