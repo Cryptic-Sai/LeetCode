e16dfee98d3078f5824a0aa2b6b41272295039e4
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-07-31 00:50:45 +0530

    Time: 319 ms (99.41%), Space: 160.9 MB (73.14%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+//     ListNode* reverse(ListNode* head){
+        
+//         if(head == NULL || head -> next ==NULL){
+//             return head;
+//         }
+//         ListNode* prev = NULL;
+//         ListNode* curr = head;
+//         ListNode* forward = NULL;
+        
+//         while(curr != NULL){
+//             forward = curr -> next  ;
+//             curr -> next = prev;
+//             prev = curr;
+//             curr = forward;
+//         }
+//         return prev;
+//     }
+      
+//     ListNode* removeNodes(ListNode* head) {
+
+//         head = reverse(head);
+//         ListNode* curr = head;
+//         int max = head -> val;
+//         ListNode* prev = head;
+//         head = head -> next;
+//         while(head != NULL){
+//             if(head -> val > max){
+//                 max =head -> val;
+//                 prev = head ;
+//                 head = head -> next;
+//             }
+//             else{
+//                 prev -> next= head -> next;
+//                 head = prev -> next;
+//             }            
+//         } 
+//         head = reverse(curr);
+//             return head;
+// }
+    
+// };
+
+    ListNode* removeNodes(ListNode* head) {
+        if (!head) return NULL;
+        head->next = removeNodes(head->next);
+        return head->next && head->val < head->next->val ?  head->next : head;
+    }
+};
