e0f2abecf69c8d45444869d1ecb7a9e0e521e059
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-07-31 01:28:48 +0530

    Time: 334 ms (98.35%), Space: 156.1 MB (95.83%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -1,51 +1,98 @@
-class Solution {
-public:
+// class Solution {
+// public:
     
-//     ListNode* reverse(ListNode* head){
+// //     ListNode* reverse(ListNode* head){
         
-//         if(head == NULL || head -> next ==NULL){
-//             return head;
-//         }
-//         ListNode* prev = NULL;
-//         ListNode* curr = head;
-//         ListNode* forward = NULL;
+// //         if(head == NULL || head -> next ==NULL){
+// //             return head;
+// //         }
+// //         ListNode* prev = NULL;
+// //         ListNode* curr = head;
+// //         ListNode* forward = NULL;
         
-//         while(curr != NULL){
-//             forward = curr -> next  ;
-//             curr -> next = prev;
-//             prev = curr;
-//             curr = forward;
-//         }
-//         return prev;
-//     }
+// //         while(curr != NULL){
+// //             forward = curr -> next  ;
+// //             curr -> next = prev;
+// //             prev = curr;
+// //             curr = forward;
+// //         }
+// //         return prev;
+// //     }
       
 //     ListNode* removeNodes(ListNode* head) {
 
-//         head = reverse(head);
-//         ListNode* curr = head;
-//         int max = head -> val;
-//         ListNode* prev = head;
-//         head = head -> next;
-//         while(head != NULL){
-//             if(head -> val > max){
-//                 max =head -> val;
-//                 prev = head ;
-//                 head = head -> next;
-//             }
-//             else{
-//                 prev -> next= head -> next;
-//                 head = prev -> next;
-//             }            
-//         } 
-//         head = reverse(curr);
-//             return head;
-// }
-    
+// //         head = reverse(head);
+// //         ListNode* curr = head;
+// //         int max = head -> val;
+// //         ListNode* prev = head;
+// //         head = head -> next;
+// //         while(head != NULL){
+// //             if(head -> val > max){
+// //                 max =head -> val;
+// //                 prev = head ;
+// //                 head = prev -> next;
+// //             }
+// //             else{
+// //                 prev -> next= head -> next;
+// //                 head = prev -> next;
+// //             }            
+// //         } 
+// //         head = reverse(curr);
+// //             return head;
+        
+//         stack<ListNode* > st;
+// }  
 // };
 
-    ListNode* removeNodes(ListNode* head) {
-        if (!head) return NULL;
-        head->next = removeNodes(head->next);
-        return head->next && head->val < head->next->val ?  head->next : head;
+class Solution
+{
+public:
+       ListNode* reverse(ListNode* head){
+        
+        if(head == NULL || head -> next ==NULL){
+            return head;
+        }
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* forward = NULL;
+        
+        while(curr != NULL){
+            forward = curr -> next  ;
+            curr -> next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    }
+ 
+    void remove(ListNode *head)
+    {
+        ListNode *prev = head;
+        int ma = head->val;
+ 
+        while (head)
+        {
+            ma = max(ma, head -> val);
+            if (head->val < ma)
+            {
+                prev->next = head->next;
+            }
+            else
+            {
+                prev = head;
+            }
+            
+            head = head -> next;
+        }
+    }
+ 
+    ListNode *removeNodes(ListNode *head)
+    {
+        head = reverse(head);
+        remove(head);
+        return reverse(head);
     }
 };
+
+
+    
