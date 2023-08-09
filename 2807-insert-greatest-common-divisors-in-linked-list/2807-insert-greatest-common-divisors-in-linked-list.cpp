3a6e42a095f9d62b444e15652f7846501360b09b
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-08-09 10:20:51 +0530

    Time: 46 ms (65.60%), Space: 30.1 MB (97.58%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
@@ -9,22 +9,32 @@
  * };
  */
 
+
+// ListNode* ptr = head;
+//     while(ptr->next){
+//         ListNode* temp = new ListNode(__gcd(ptr->val, ptr->next->val),  ptr->next);
+//         ptr->next = temp;
+//         ptr = temp->next;
+//     }
+//     return head;
+// }
+
 class Solution {
 public:
     
-    int gcd(int a, int b){
-        if(b==0) return a;
-        return gcd(b,a%b);
-    }
+    // int gcd(int a, int b){
+    //     if(b==0) return a;
+    //     return gcd(b,a%b);
+    // }
     
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
-        if(head == NULL || head -> next == NULL){
-            return head;;
-        }
+//         if(head == NULL || head -> next == NULL){
+//             return head;;
+//         }
         
         ListNode* temp = head;
         while(temp -> next){
-            ListNode* ans = new ListNode(gcd(temp -> val , temp -> next -> val));
+            ListNode* ans = new ListNode(__gcd(temp -> val , temp -> next -> val), temp -> next);
             ListNode* prev = temp -> next;
             temp -> next = ans;
             ans -> next = prev;
