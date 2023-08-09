5571002744f3a3aecde4365bfdc048487030662e
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-08-09 11:33:09 +0530

    Time: 3 ms (99.82%), Space: 8.1 MB (62.84%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -25,17 +25,16 @@ public:
         if(head == NULL)
             return false;
 
-        ListNode *fast = head;
-        ListNode *slow = head;
+        ListNode* fast = head;
+        ListNode* slow = head;
         
-       while(fast != NULL && fast -> next != NULL){
-           fast = fast -> next -> next;
-           slow = slow -> next;
-           
-           if(fast == slow)
-               return true;
-              
-       }
-        return false;
+        while(fast != NULL && fast -> next != NULL){      
+            fast = fast -> next -> next;
+            slow = slow -> next;
+        
+        if(fast == slow)
+            return true;
+}
+         return false;
 }
 };
