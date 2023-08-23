f0b8524abf115ea23b9f2f02cd4103c504935020
C Sai Anand <80689926+CrypticSai-08@users.noreply.github.com>
2023-08-23 14:57:12 +0530

    Time: 60 ms (61.50%), Space: 32.1 MB (67.93%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    // vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-    //     sort(arr.begin(), arr.end());
-    //     vector<int> ans;
-    //     for(int i =0; i< arr.size(); i++){
-    //         11
-    //     }
-    // }
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-    sort(arr.begin(), arr.end());
-    
-    vector<vector<int>> result;
-    int min_diff = INT_MAX;
-    
-    // find min difference
-    for (int i = 0; i < arr.size() - 1; ++i) {
-        min_diff = min(arr[i + 1] - arr[i], min_diff);
+        sort(arr.begin(), arr.end());
+        // vector<int> ans;
+        vector<vector<int>> ans;
+        int min_diff = INT_MAX;
+        for(int i =0; i< arr.size() -1; i++){
+            min_diff = min(arr[i+1] - arr[i] , min_diff);
+        }
+        for(int i=0; i< arr.size() -1; i++){
+            if(arr[i+1] - arr[i] == min_diff){
+                ans.push_back({arr[i], arr[i+1]});
+            }
+        }
+        return ans;
+        
     }
-    
-    // form a list of pairs with min difference, ascending  
-    for (int i = 0; i < arr.size() - 1; ++i) {
-        if (arr[i + 1] - arr[i] == min_diff)
-            result.push_back({arr[i], arr[i + 1]});
-    }
-    return result;        
-}
+       
 };
