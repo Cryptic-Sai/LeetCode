f70b49d68a627c2916ee5b6d7de8313b05c09b15
CrypticSai <80689926+CrypticSai-08@users.noreply.github.com>
2023-11-19 18:06:59 +0530

    Time: 150 ms (87.12%), Space: 83.1 MB (88.96%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1887-reduction-operations-to-make-the-array-elements-equal/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+    // map<int,int> mp;
+    // int n = nums.size();
+    sort(nums.begin(), nums.end());
+    int ans =0;
+    int j =0;
+    for(int i=1; i<nums.size(); i++){
+        if(nums[i] != nums[i-1]){
+            j++;
+        }
+        
+            ans +=j;
+        
+    }return ans;
+    
+}
+};
