134c37dfa9917fe87a05c6389467dfe18844a947
CrypticSai <80689926+CrypticSai-08@users.noreply.github.com>
2024-03-08 15:51:22 +0530

    Time: 3 ms (89.80%), Space: 15.8 MB (79.43%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,34 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> result(2, -1); // Initializing result vector with -1
-
-        // Finding the leftmost index of the target
-        int s = 0, e = nums.size() - 1;
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] == target) {
-                result[0] = mid; // Update leftmost index
-                e = mid - 1; // Continue searching towards left for the leftmost occurrence
-            } else if (nums[mid] < target) {
-                s = mid + 1;
-            } else {
-                e = mid - 1;
+        
+        vector<int>res(2, -1);
+        int s =0;
+        int e = nums.size()-1;
+        while(s<=e){     
+            int mid = s + (e-s)/2;
+            if(nums[mid] == target){
+                res[0] = mid;
+                e = mid -1;
+            }else if(nums[mid] < target){
+                s = mid+1;
+            }else{
+                e = mid-1;
             }
         }
-
-        // Finding the rightmost index of the target
-        s = 0, e = nums.size() - 1; // Resetting s and e pointers
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] == target) {
-                result[1] = mid; // Update rightmost index
-                s = mid + 1; // Continue searching towards right for the rightmost occurrence
-            } else if (nums[mid] < target) {
-                s = mid + 1;
-            } else {
-                e = mid - 1;
+        s =0;
+        e = nums.size()-1;
+        while(s<=e){     
+            int mid = s + (e-s)/2;
+            if(nums[mid] == target){
+                res[1] = mid;
+                s = mid+1;
+            }else if(nums[mid] < target){
+                s = mid+1;
+            }else{
+                e = mid-1;
             }
         }
-
-        return result;
+        return res;
     }
 };
