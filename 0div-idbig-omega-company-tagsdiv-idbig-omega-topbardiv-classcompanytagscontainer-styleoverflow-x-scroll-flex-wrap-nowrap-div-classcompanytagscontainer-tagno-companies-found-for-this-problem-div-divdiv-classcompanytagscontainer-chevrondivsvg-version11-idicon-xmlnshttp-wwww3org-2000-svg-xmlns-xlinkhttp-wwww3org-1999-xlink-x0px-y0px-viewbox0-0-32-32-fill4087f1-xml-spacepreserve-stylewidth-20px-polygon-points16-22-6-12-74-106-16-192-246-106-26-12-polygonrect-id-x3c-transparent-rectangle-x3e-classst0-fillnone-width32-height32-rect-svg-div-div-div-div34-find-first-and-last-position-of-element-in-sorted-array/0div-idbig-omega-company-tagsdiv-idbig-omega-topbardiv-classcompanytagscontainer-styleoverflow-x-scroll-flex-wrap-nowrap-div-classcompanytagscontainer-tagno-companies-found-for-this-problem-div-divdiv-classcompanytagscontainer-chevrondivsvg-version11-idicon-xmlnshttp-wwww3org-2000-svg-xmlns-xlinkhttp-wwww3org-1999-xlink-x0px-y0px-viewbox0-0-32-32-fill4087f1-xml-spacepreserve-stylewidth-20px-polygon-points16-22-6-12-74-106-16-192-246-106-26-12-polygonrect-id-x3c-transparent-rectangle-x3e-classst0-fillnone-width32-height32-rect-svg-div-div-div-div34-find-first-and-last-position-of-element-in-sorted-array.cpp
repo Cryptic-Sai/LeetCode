0ddea0138bc6a5e8f84e570d06f52b7abb94df01
CrypticSai <80689926+CrypticSai-08@users.noreply.github.com>
2024-03-08 15:42:00 +0530

    Time: 4 ms (64.26%), Space: 16.2 MB (10.08%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,23 +1,73 @@
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& nums, int target) {
+        
+//         vector<int>res(2, -1);
+//         int s =0;
+//         int e = nums.size()-1;
+//         while(s<=e){     
+//             int mid = s + (e-s)/2;
+//             if(nums[mid] == target){
+//                 res[0] = mid;
+//                 e = mid -1;
+//             }if(nums[mid] < target){
+//                 s = mid+1;
+//             }else{
+//                 e = mid-1;
+//             }
+//         }
+//         s =0;
+//         e = nums.size()-1;
+//         while(s<=e){     
+//             int mid = s + (e-s)/2;
+//             if(nums[mid] == target){
+//                 res[1] = mid;
+//                 s = mid+1;
+//             }if(nums[mid] < target){
+//                 s = mid+1;
+//             }else{
+//                 e = mid-1;
+//             }
+//         }
+//         return res;
+//     }
+// };
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-    int idx1 = lower_bound(nums, target);
-    int idx2 = lower_bound(nums, target+1)-1;
-    if (idx1 < nums.size() && nums[idx1] == target)
-        return {idx1, idx2};
-    else
-        return {-1, -1};
-}
+        vector<int> result(2, -1); // Initializing result vector with -1
 
-int lower_bound(vector<int>& nums, int target) {
-    int l = 0, r = nums.size()-1;
-    while (l <= r) {
-        int mid = (r-l)/2+l;
-        if (nums[mid] < target)
-            l = mid+1;
-        else
-            r = mid-1;
+        // Finding the leftmost index of the target
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result[0] = mid; // Update leftmost index
+                right = mid - 1; // Continue searching towards left for the leftmost occurrence
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        // Finding the rightmost index of the target
+        left = 0, right = nums.size() - 1; // Resetting left and right pointers
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                result[1] = mid; // Update rightmost index
+                left = mid + 1; // Continue searching towards right for the rightmost occurrence
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
     }
-    return l;
-}
 };
